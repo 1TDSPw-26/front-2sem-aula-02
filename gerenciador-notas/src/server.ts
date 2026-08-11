b9a2d880efd676e6b01fd36411103f0cfa81a996
Gabriel-Oliveira0611 <gabriel.pereira.0694@outlook.com>
2026-08-11 18:03:47 -0300

    1 aula

---
+++ gerenciador-notas/src/server.ts
@@ -0,0 +1,42 @@
+import express, { Request, response, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+const DB_URL = "http://localhost:3001/alunos";
+
+interface Aluno {
+  id: string;
+  nome: string;
+  nota1: number;
+  nota2: number;
+}
+
+// Permitir que o express receba o tipo .json
+app.use(express.json());
+
+// Disponibilizando arquivos na pasta public
+app.use(express.static("public"));
+
+// Rota para consular os alunos
+app.get("/api/alunos", async (_req: Request, res: Response) => {
+  try {
+    const response = await fetch(DB_URL);
+
+    if (!response.ok) {
+      throw new Error("Não foi possível consultar os alunos!");
+    }
+
+    const alunos = (await response.json()) as Aluno[];
+
+    res.json(alunos);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({ error: "Não foi possível consultar os alunos!" });
+  }
+});
+
+// Criando um listen na porta disponibilizada
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
